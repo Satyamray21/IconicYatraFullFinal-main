6c0d6e15298582d50da571e06774796fa11f1462
makazsatyam-create <mazaksatyam@gmail.com>
2026-04-16 22:50:54 +0530

    FIX:-ALL ISSUE OF STAFF.

--- backend/src/models/staff.model.js
+++ backend/src/models/staff.model.js
@@ -1,15 +1,17 @@
 import mongoose from "mongoose";
 import { addressSchema } from "../common/address.common.js";
-import { firmSchema } from "../common/firm.common.js";
 import { bankSchema } from "../common/bankDetails.common.js";
+
 const staffSchema = new mongoose.Schema(
   {
     personalDetails: {
+      title:{
+        type:String
+      },
       fullName: {
         type: String,
         required: true,
       },
-
       mobileNumber: {
         type: String,
         required: true,
@@ -17,7 +19,7 @@ const staffSchema = new mongoose.Schema(
       },
       alternateContact: {
         type: String,
-        unique: true,
+        
       },
       designation: {
         type: String,
@@ -32,6 +34,32 @@ const staffSchema = new mongoose.Schema(
         type: String,
         required: true,
         unique: true,
+      },
+      aadharNumber: {
+        type: String,
+        unique: true,
+        sparse: true,
+      },
+      panNumber: {
+        type: String,
+        unique: true,
+        sparse: true,
+      },
+      dob:{
+        type: Date,
+      },
+      // Optional photo fields
+      staffPhoto: {
+        url: String,
+        publicId: String,
+      },
+      aadharPhoto: {
+        url: String,
+        publicId: String,
+      },
+      panPhoto: {
+        url: String,
+        publicId: String,
       },
     },
     staffId: {
@@ -53,9 +81,7 @@ const staffSchema = new mongoose.Schema(
         required: true,
       },
     },
-
     address: addressSchema,
-    firm: firmSchema,
     bank: bankSchema,
   },
   { timestamps: true },
