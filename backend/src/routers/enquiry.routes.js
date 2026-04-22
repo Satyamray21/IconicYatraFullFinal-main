938590e2bee92de75daaf9d252d47e318fdfb52b
Satyamray21 <satyamray43@gmail.com>
2026-04-22 21:53:42 +0530

    fix:-mutiple detele options.

--- backend/src/routers/enquiry.routes.js
+++ backend/src/routers/enquiry.routes.js
@@ -1,10 +1,18 @@
 import express from "express";
-import { createEnquiry,getAllEnquiries,updateEnquiryStatus } from "../controllers/enquiry.controller.js";
+import {
+  createEnquiry,
+  getAllEnquiries,
+  updateEnquiryStatus,
+  deleteEnquiry,
+  deleteMultipleEnquiries,
+} from "../controllers/enquiry.controller.js";
 
 const router = express.Router();
 
 router.post("/create", createEnquiry);
 router.get("/admin/enquiries", getAllEnquiries);
 router.patch("/admin/enquiry/:id", updateEnquiryStatus);
+router.delete("/enquiry/:id", deleteEnquiry);
+router.post("/enquiry/delete-multiple", deleteMultipleEnquiries);
 
 export default router;
