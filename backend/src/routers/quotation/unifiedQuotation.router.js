826f4ab730e57ee5de3bdb4e43aaaf6cadc0736f
Satyamray21 <satyamray43@gmail.com>
2026-06-24 00:28:47 +0530

    fix:-added the hotel avaibliry configuration

--- backend/src/routers/quotation/unifiedQuotation.router.js
+++ backend/src/routers/quotation/unifiedQuotation.router.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { searchAllQuotations, getUnifiedQuotationStats, getPaymentSummary } from "../../controllers/quotation/unifiedQuotation.controller.js";
+import { searchAllQuotations, getUnifiedQuotationStats, getPaymentSummary, getUpcomingStayLocations, previewHotelAvailabilityEmail, sendHotelAvailabilityEmail } from "../../controllers/quotation/unifiedQuotation.controller.js";
 
 const router = Router();
 
 router.get("/search", searchAllQuotations);
 router.get("/stats", getUnifiedQuotationStats);
 router.get("/payment-summary", getPaymentSummary);
+
+router.get("/stay-locations", getUpcomingStayLocations);
+router.post("/hotel-availability-email/preview", previewHotelAvailabilityEmail);
+router.post("/hotel-availability-email/send", sendHotelAvailabilityEmail);
 
 export default router;
