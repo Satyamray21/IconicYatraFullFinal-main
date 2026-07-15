067eb929e5f3bf72176d3e05f95710d5754c34ca
Satyamray21 <satyamray43@gmail.com>
2026-07-15 22:50:41 +0530

    fix:-remove console

--- backend/src/config/razorpay.js
+++ backend/src/config/razorpay.js
@@ -1,5 +1,5 @@
 import Razorpay from "razorpay";
-console.log("KEY ID:", process.env.RAZORPAY_KEY_ID);
+
 const razorpayInstance = new Razorpay({
     key_id: process.env.RAZORPAY_KEY_ID,
     key_secret: process.env.RAZORPAY_KEY_SECRET
