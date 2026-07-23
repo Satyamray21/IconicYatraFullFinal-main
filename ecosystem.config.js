b8dd089e2427d9ff84dd4f8a66f8500d2a9a519c
makazsatyam-create <mazaksatyam@gmail.com>
2026-07-23 15:49:58 +0530

    fix:-name

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   apps: [
     {
-      name: "iconicyatra-api",
+      name: "globevisitors-api",
       script: "index.js",
-      cwd: "/var/www/iconicyatra/backend",
+      cwd: "/var/www/api.globevisitors.com/backend",
       instances: "max",       // Run in cluster mode using all CPU cores
       exec_mode: "cluster",   // Cluster mode for zero-downtime reloads
       watch: false,           // Do not watch in production
       max_memory_restart: "1G", // Restart if it uses too much memory
       env_production: {
         NODE_ENV: "production",
-        PORT: 3000
+        PORT: 3001
       }
     }
   ]
